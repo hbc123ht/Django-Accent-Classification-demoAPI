2731decc53554e09d51d03fb4b64f7b654e9a408
hbc123ht <hbc123ht@gmail.com>
2021-06-17 16:02:51 +0700

    automatically show result without reloading

--- ML/views.py
+++ ML/views.py
@@ -1,31 +1,31 @@
 from django.shortcuts import render, redirect
+from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
 import wave
 import os
 from ML import Accent_Identification
 from ML.models import Result
+from django.core.files.storage import default_storage
+from django.core.files.base import ContentFile
 
-result = Result()
 ML = Accent_Identification()
 # Create your views here.
 def index(request):
-    ctx = {
-        'result' : result,
-    }
-    return render(request, 'index.html', ctx)
+    return render(request, 'index.html')
 
 @csrf_exempt
 def predict(request):
-    uploadedFile = open("test.wav", "wb")
+    # uploadedFile = open("test.wav", "wb")
+    # uploadedFile.write(request.body)
+    # uploadedFile.close()
     # the actual file is in request.body
-    uploadedFile.write(request.body)
-    uploadedFile.close()
-    
-    result.result = ML.predict('test.wav')
-    os.remove('test.wav')
 
-    return redirect('/')
-
-    
-
-    return redirect('/')
+    audio_data = request.FILES['audio']
+    default_storage.save('voice.wav', ContentFile(audio_data.read()))
+    result = None
+    try:
+        result = ML.predict('voice.wav')
+    except:
+        pass
+    os.remove('voice.wav')
+    return JsonResponse({'result':result})
